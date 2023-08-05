d2c3d7527f33d62cf51667d9b9f65fd8a29bda1f
Artem <moiseev@physics.msu.ru>
2023-08-05 10:49:51 +0300

    Initial Commit

---
+++ Project7-iExpense(100SUI)/Project7_iExpense_100SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Project7_iExpense_100SUI_App.swift
+//  Project7-iExpense(100SUI)
+//
+//  Created by Artem on 05.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project7_iExpense_100SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
